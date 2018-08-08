8720326b2104adabcf12403a28a2c75fb3e9f0f3
Suzanne Huldt <suzannegchuldt@gmail.com>
2018-08-08 19:38:30 +0100

    basic algorithm

---
+++ main.rb
@@ -0,0 +1,16 @@
+class Main
+  def element1(num)
+    operand1 = Array.new(num)
+    return operand1
+  end
+
+  def element2(num)
+    operand2 = Array.new(num)
+    return operand2
+  end
+
+  def add(operand1, operand2)
+    result = operand2.concat(operand2)
+    return result
+  end
+end
